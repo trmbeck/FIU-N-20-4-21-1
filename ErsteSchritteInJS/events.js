233284d60756e986ed5d304e4e23e49375c3a737
mbeck-tr <mbeck@lutz-und-grub.de>
2021-10-13 11:39:44 +0200

    events hinzugefügt

--- ErsteSchritteInJS/events.js
+++ ErsteSchritteInJS/events.js
@@ -1,3 +1,8 @@
+let mailElement = document.getElementById("txbEmail");
+mailElement.addEventListener('blur',mailBlurEventHandler,false); //false = bubbling, true = capturing
+mailElement.addEventListener('blur',mailBlur,false);
+
+
 function changeColorOnMouseOut(){
     let control = document.getElementById("txbName");
     control.style.background = 'red';
@@ -6,4 +11,33 @@ function changeColorOnMouseOut(){
     control = document.getElementById("btn");
     control.style.background = 'black';
     control.style.color = "white";
+}
+
+function senden(){
+    //Zugriff auf DOM-Elemente
+    let txbname = document.getElementById("txbName");
+    let txbemail = document.getElementById("txbEmail");
+    txbname.value = "Bitte Name eingeben";
+    txbemail.value = "Bitte E-Mail eingeben";
+    let name = document.getElementsByName("Name");
+    console.log(name[0]);
+    for (let index = 0; index < name.length; index++) {
+        const element = name[index];
+        console.log(element.id);
+    }
+    let tdListe = document.getElementsByTagName("td");
+    console.log(tdListe);
+    for (let index = 0; index < tdListe.length; index++) {
+        const element = tdListe[index];
+        console.log(element.innerHTML);
+    }
+}
+
+function mailBlur(){
+    console.log("Blur");
+    mailElement.removeEventListener('blur',mailBlur,false);
+}
+
+function mailBlurEventHandler(){
+    console.log("BlurEventHandler");
 }
